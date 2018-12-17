d08270701d57248354209e2e6d0b497d8231fcdf
Longhao Chen <lc18@bu.edu>
2018-12-16 23:57:54 -0500

    .

--- changepoints/Scripts/summary statistics.R
+++
@@ -1,7 +0,0 @@
-sum.stat <- function(data){
- mean <- mean(data) 
- standard.dev <- sd(data)
- skewness <- skewness(data)
- kurtosis <- kurtosis(data)
- rbind(mean,standard.dev,skewness,kurtosis)
-} 
